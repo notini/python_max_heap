e488543d581296b76219ee16b9e02db2e42f4265
Vitor Notini Pontes <vitornotini@gmail.com>
2018-08-26 22:29:00 -0300

    Example cleaning

--- example.py
+++ example.py
@@ -1,15 +1,16 @@
 import math
+import random
 from maxHeap import MaxHeap
 
 max_heap = MaxHeap()
 
-#Initializing heap. You decide what to use, this is just an example.
-#heap = [random.randint(0,20) for i in range(10)]
-heap = [4,1,3,2,16,9,10,14,8,7]
-for idx in range(math.floor(len(heap) / 2), 0, -1):
-	max_heap.max_heapify(heap, len(heap), idx - 1)
-	
+values = [random.randint(0,20) for i in range(10)]
+heap = []
+
+#Initializing heap with values from a random array. 
 print('Values inserted to the heap:')
+for value in values:
+	max_heap.insertKey(heap, value)
 print(heap)
 
 print('Value on index 6 (if 6 is a valid index) now equals 15')
